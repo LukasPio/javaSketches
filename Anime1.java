78dc46b6e7b00056d697175d46ae9dc3ca29fd7c
lucas <lucaspio.galvao@gmail.com>
2023-08-08 20:30:41 -0300

    PrimeiroPrograma

---
+++ Anime1.java
@@ -0,0 +1,41 @@
+package cursoDeJava.exercicios.POO.rascunhos.fds;
+
+public class Anime1 {
+    //Ordem de procedimentos após a criação de um objeto
+
+    // 1- Alocação de espaço em memória
+    // 2- Cada atributo de classe é criado e inicializado com os valores default ou o quer for passado
+    // 3- Bloco de inicialização é executado
+    // 4- Construtor é executado
+
+    private String nome = "OneRandomAnime\n";
+    private int[] episodios;
+
+    {
+        System.out.println("\nDentro do bloco de inicialização");
+        this.episodios = new int[100];
+
+        for (int i = 0; i < this.episodios.length; i++) {
+            this.episodios[i] = i + 1;
+        }
+
+        for (int episodio: this.episodios) {
+            System.out.print(episodio + " ");
+        }
+        System.out.println("\n");
+        System.out.println("Fora do bloco de inicialização: \n");
+    }
+
+
+    public Anime1(String nome) {
+        this.nome = nome;
+    }
+
+    public Anime1 () {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+}
---
+++ Anime1Test.java
@@ -0,0 +1,8 @@
+package cursoDeJava.exercicios.POO.rascunhos.fds;
+
+public class Anime1Test {
+    public static void main(String[] args) {
+        Anime1 anime = new Anime1();
+        System.out.println(anime.getNome());
+    }
+}
