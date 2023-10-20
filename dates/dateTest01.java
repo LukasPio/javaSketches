e1c11f3e25d590c8da3029f2c562f015a4d786c3
lucas <lucaspio.galvao@gmail.com>
2023-10-19 22:28:42 -0300

    "começando os estudos sobre datas "

---
+++ dates/dateTest01.java
@@ -0,0 +1,11 @@
+package cursoDeJava.exercicios.POO.rascunhos.fds.dates;
+
+import java.util.Date;
+
+public class dateTest01 {
+    public static void main(String[] args) {
+        Date date = new Date(1697765111313L);
+
+        System.out.println(date);
+    }
+}
