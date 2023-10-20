03c72c2bd748fe20b62f3935bd9e9803b4f667a2
lucas <lucaspio.galvao@gmail.com>
2023-10-19 22:19:31 -0300

    finalização dos estudos sobre Strings, StringBuffers e StringBuilders

--- Strings/test/StringTest03.java
+++ Strings/test/StringTest03.java
@@ -3,8 +3,16 @@ package cursoDeJava.exercicios.POO.rascunhos.fds.Strings.test;
 public class StringTest03 {
     public static void main(String[] args) {
         String nome = "Lucas Pio";
-        nome.concat(" - DEV");
+        nome.concat(" DEV");
+        nome.substring(0, 3);
         System.out.println(nome);
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder("Lucas Pio");
+        sb.append(" - DEV").append(" Back-end").append(" em Java");
+        sb.reverse();
+        sb.reverse();
+        sb.delete(0, 3);
+        System.out.println(sb);
+
+        // StringBuilders / StringBuffers são mutáveis e por isso tem um maior desempenho
     }
 }
